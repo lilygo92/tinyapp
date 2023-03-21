ba41f799879047e5a63988352a83e2910faffa7e
Lily Go <lilygo7192@gmail.com>
2023-03-21 11:02:36 -0700

    Fixed random string function returning errors

--- express_server.js
+++ express_server.js
@@ -13,8 +13,8 @@ const urlDatabase = {
 
 
 generateRandomString = () => {
-    const buf = crypto.randomBytes(6);
-    return buf.toString("hash");
+    const buf = crypto.randomBytes(3);
+    return buf.toString("hex");
 }
 
 app.use(express.urlencoded({ extended: true }));
@@ -38,17 +38,17 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
-})
+});
 
 app.post("/urls", (req, res) => {
   console.log(req.body); 
-  res.send("Ok"); 
+  res.send(generateRandomString()); 
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
